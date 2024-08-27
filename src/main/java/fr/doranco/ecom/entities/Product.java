b0ea965561f29338481d2ac8268203ef066e5012
Maxime <maxime.b2494@gmail.com>
2024-08-27 17:05:08 +0200

    feature: search by name

--- src/main/java/fr/doranco/ecom/entities/Product.java
+++ src/main/java/fr/doranco/ecom/entities/Product.java
@@ -49,6 +49,7 @@ public class Product {
         productDto.setDescription(description);
         productDto.setByteImg(img);
         productDto.setCategoryId(category.getId());
+        productDto.setCategoryName(category.getName());
         return productDto;
     }
 }
